162260f00877142da33263053df23f78d65fd84a
OhMinSeop <mins5190@gmail.com>
2023-02-01 23:08:17 +0900

    add: sst 환경 변수 없으면 Error throw

--- stacks/NextApp.ts
+++ stacks/NextApp.ts
@@ -3,7 +3,14 @@ import { NextjsSite, StackContext } from 'sst/constructs';
 export function NextApp({ stack }: StackContext) {
   const NEXT_APP_PATH = '.';
   const S3_BUCKET_NAME = process.env.AWS_S3_BUCKET;
-  const AWS_SST_ID = process.env.AWS_SST_ID || 'SST_NEXTJS_WEB';
+  const AWS_SST_ID = process.env.AWS_SST_ID;
+
+  if (!S3_BUCKET_NAME || !AWS_SST_ID) {
+    const error = new Error();
+    error.name = 'MissingEnvVars';
+    error.message = 'Missing "AWS_S3_BUCKET" or "AWS_SST_ID"';
+    throw error;
+  }
 
   const site = new NextjsSite(stack, AWS_SST_ID, {
     path: NEXT_APP_PATH,
