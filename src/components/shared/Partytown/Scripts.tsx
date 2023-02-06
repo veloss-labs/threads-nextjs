455f8960ef7eacfdf98491dd057a2a8506c7ba60
OhMinSeop <msoh@2online.io>
2023-02-06 15:18:49 +0900

    add: pwa를 위한 작업중

--- src/components/shared/Partytown/Scripts.tsx
+++ src/components/shared/Partytown/Scripts.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Script from 'next/script';
 import { Partytown } from '@builder.io/partytown/react';
-import { deployGroup, environment, gaTrackingID } from '~/constants/env';
+import { deployGroup, gaTrackingID } from '~/constants/env';
 
 const Scripts = () => {
-  const renderGaScript = () => {
-    if (!gaTrackingID) return null;
-    return (
-      <>
-        <Script
-          id="ga-script"
-          type="text/partytown"
-          src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingID}`}
-        />
-        <Script
-          id="ga-script"
-          type="text/partytown"
-          dangerouslySetInnerHTML={{
-            __html: `
+  const disabled = useMemo(() => !gaTrackingID || deployGroup === 'local', []);
+  // const disabled = useMemo(() => false, []);
+
+  if (disabled) {
+    return null;
+  }
+
+  return (
+    <>
+      <Partytown debug={deployGroup === 'local'} forward={['dataLayer.push']} />
+      <Script
+        id="ga-script"
+        type="text/partytown"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingID}`}
+      />
+      <Script
+        id="ga-script"
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -25,24 +31,14 @@ const Scripts = () => {
               page_path: window.location.pathname,
             });
           `,
-          }}
-        />
-      </>
-    );
-  };
-
-  const renderPartytownScript = () => {
-    if (!gaTrackingID) return null;
-
-    return (
-      <Partytown debug={deployGroup === 'local'} forward={['dataLayer.push']} />
-    );
-  };
-
-  return (
-    <>
-      {renderPartytownScript()}
-      {renderGaScript()}
+        }}
+      />
+      {/* <Script
+        id="sw-script"
+        type="text/partytown"
+        src="/sw.js"
+        data-service-worker="true"
+      /> */}
     </>
   );
 };
